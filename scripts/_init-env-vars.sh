32b7ba435fd1d6d0d0fe7d07ff9e7f1f42c0eb33
Madleine Knapprecht <it@astzweig.de>
2018-08-17 08:40:54 +0200

    Refactor _init-env-vars.sh script
    The script only handles the declaration of variables. Dividing it in
    multiple functions prooved to be badly readable and did not provide any
    additional clarification. So we use only one function and divide the
    variables in section using comments.

--- scripts/_init-env-vars.sh
+++ scripts/_init-env-vars.sh
@@ -1,47 +1,46 @@
 #!/bin/sh
 
 init_runtime_env_variables () {
+    # Initialize environment variables. Variables prefixed with an
+    # underscore are 'calculated' variables. That means their value is
+    # inferred by the values of other variables.
 
-    _init_general_env_vars;
-    _init_api_env_vars;
-    _init_configprofile_env_vars;
-    _init_imap_env_vars;
-    _init_calculated_vars;
-}
-
-
-_init_calculated_vars () {
-    _init_redis_calc_vars;
-    _init_graylog_calc_vars;
-}
-
-
-_init_general_env_vars () {
+    # === General ===
     # Simple (too general) domain recognizing regular expression.
     local _DOMAIN_REGEX='[^[:space:]]\{1,63\}\.\+[^[:space:].]\+$';
     _check_value 'FQDN' "${_DOMAIN_REGEX}" 'exit';
     _check_value 'MAIL_DOMAIN' "${_DOMAIN_REGEX}" "${FQDN}";
     _check_value 'PRODUCT_NAME' '.\+' 'Wildduck Mail';
     _check_value 'MONGODB_HOST' '.\+' 'mongodb://mongodb:27017/wildduck';
-    _check_value 'REDIS_HOST' '.\+' 'redis://redis:6379/8';
 
+    # === General: Redis ===
+    _check_value 'REDIS_HOST' '.\+' 'redis://redis:6379/8';
+    # Split REDIS_HOST into components as we need the components in the
+    # configuration files.
+    export _REDIS_PORT="$(_get_url_part "${REDIS_HOST}" port)";
+    export _REDIS_HOSTNAME="$(_get_url_part "${REDIS_HOST}" hostname)";
+    export _REDIS_DB="$(_get_url_part "${REDIS_HOST}" path)";
+
+    # === General ===
     export TLS_KEY="${TLS_KEY}";
     export TLS_CERT="${TLS_CERT}";
+
+    # === General: Graylog ===
     export GRAYLOG_HOST_PORT="${GRAYLOG_HOST_PORT}";
-}
+    export _GRAYLOG_PORT="$(_get_url_part "${GRAYLOG_HOST_PORT}" port)";
+    export _GRAYLOG_HOSTNAME="$(_get_url_part "${GRAYLOG_HOST_PORT}" \
+        hostname)";
 
-
-_init_api_env_vars () {
+    # === API ===
     local PROTO='http';
     _check_value 'API_ENABLE' 'true\|false' 'true';
     _check_value 'API_USE_HTTPS' 'true\|false' 'true';
     [ "${API_USE_HTTPS}" = 'true' ] && PROTO="${PROTO}s";
     _check_value 'API_URL' '.\+' "${PROTO}://${FQDN}";
     _check_value 'API_TOKEN_SECRET' '.\+' "$(_get_random_string)";
-}
 
 
-_init_configprofile_env_vars () {
+    # === Configprofile ===
     # default identifier for mobilconfig is the first two parts of the
     # reversed FQDN with '.wildduck' appended.
     local REV_FQDN="$(echo $FQDN | \
@@ -52,27 +51,10 @@ _init_configprofile_env_vars () {
     _check_value 'CONFIGPROFILE_DISPLAY_DESC' '.\+' \
         'Install this profile to setup {email}';
     _check_value 'CONFIGPROFILE_ACCOUNT_DESC' '.\+' '{email}';
-}
 
 
-_init_imap_env_vars () {
+    # === IMAP ===
     _check_value 'IMAP_PROCESSES' '[[:digit:]]\+$' '2';
     _check_value 'IMAP_RETENTION' '[[:digit:]]\+$' '4';
     _check_value 'IMAP_DISABLE_STARTTLS' 'true\|false' 'false';
-}
-
-
-_init_redis_calc_vars () {
-    # Split REDIS_HOST into components as we need the components in the
-    # configuration files.
-    export _REDIS_PORT="$(_get_url_part "${REDIS_HOST}" port)";
-    export _REDIS_HOSTNAME="$(_get_url_part "${REDIS_HOST}" hostname)";
-    export _REDIS_DB="$(_get_url_part "${REDIS_HOST}" path)";
-}
-
-
-_init_graylog_calc_vars () {
-    export _GRAYLOG_PORT="$(_get_url_part "${GRAYLOG_HOST_PORT}" port)";
-    export _GRAYLOG_HOSTNAME="$(_get_url_part "${GRAYLOG_HOST_PORT}" \
-        hostname)";
 }
