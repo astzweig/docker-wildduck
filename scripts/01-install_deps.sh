7f87b175d1a91185b004c453094a8327a9635be1
Madleine Knapprecht <it@astzweig.de>
2018-08-20 11:47:06 +0200

    Install python3-dev (for ruaml compilation)

--- scripts/01-install_deps.sh
+++ scripts/01-install_deps.sh
@@ -3,7 +3,7 @@ apk add --no-cache nodejs;  # needed to run services.
 apk add --no-cache rspamd clamav;  # antispam tools.
 apk add --no-cache openssl;  # needed to generate dkim keys at runtime.
 apk add --no-cache curl;  # needed to run API requests.
-apk add --no-cache --virtual build-deps git python npm make g++;
+apk add --no-cache --virtual build-deps git python python3-dev npm make g++;
 
 apk add --no-cache python3;  # needed to run cocof tool, to edit
 pip3 install cocof;          # configuration files.
